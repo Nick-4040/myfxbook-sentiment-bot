d6c673774eadda97f57c6f2612217d99d985199d
Nick-4040 <espritelibre42@gmail.com>
2026-01-05 12:10:04 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -2,15 +2,17 @@ import requests
 import os
 from datetime import datetime
 
-print("TOKEN presente:", TELEGRAM_TOKEN is not None)
-print("CHAT ID:", CHAT_ID)
-
 
 MYFXBOOK_URL = "https://www.myfxbook.com/api/get-community-outlook.json"
 THRESHOLD = 65
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+print("TOKEN presente:", TELEGRAM_TOKEN is not None)
+print("CHAT ID:", CHAT_ID)
+
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
